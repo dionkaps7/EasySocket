718eb22150b4c1597440000de7efd8a003053892
₱ØⱠɎ₥ØⱤ₱Ⱨł₵ <54730301+lilmond@users.noreply.github.com>
2025-05-20 09:59:21 +0000

    Update client.py

--- client.py
+++ client.py
@@ -1,79 +1,11 @@
-from typing import Callable, Optional
+from peer import EasySocketPeer
+from typing import Callable
 import threading
 import socket
 
 
-class EasySockClientSocket(object):
-    def __init__(self, sock: socket.socket, suffix: bytes):
-        self.event_messaged = threading.Event()
-        self.closed = False
-
-        self.sock = sock
-        self.suffix = suffix
-        self.on_message: Optional[Callable[[bytes], None]] = None
-        self.on_disconnect: Optional[Callable[[], None]] = None
-    
-        threading.Thread(target=self._listen, daemon=True).start()
-
-    def _listen(self):
-        try:
-            while True:
-                if self.on_message:
-                    self.event_messaged.clear()
-                    message = self.recv_message()
-                    if not message and self.closed:
-                        break
-                    self.on_message(message)
-                    self.event_messaged.set()
-        except Exception:
-            self.closed = True
-
-            if self.on_disconnect:
-                self.on_disconnect()
-    
-    def close(self):
-        if not self.closed:
-            self.closed = True
-
-            try:
-                self.sock.shutdown(socket.SHUT_RDWR)
-            except Exception:
-                pass
-
-            try:
-                self.sock.close()
-            except Exception:
-                pass
-            
-            if self.on_disconnect:
-                self.on_disconnect()
-    
-    def recv_message(self):
-        message = b""
-
-        while True:
-            chunk = self.sock.recv(1)
-            if not chunk:
-                self.closed = True
-                if self.on_disconnect:
-                    self.on_disconnect
-                return b""
-
-            message += chunk
-            if message.endswith(self.suffix):
-                break
-        
-        return message[:-len(self.suffix)]
-
-    def send_message(self, message: bytes):
-        self.sock.send(message + self.suffix)
-
-    def wait_message(self):
-        self.event_messaged.wait()
-
-
-class EasySockClient(object):
-    def __init__(self, host: str, port: int, on_connect: Callable[[EasySockClientSocket], None], suffix: bytes = b"\x00\x00\x00\x00\x00", timeout: int = None, threaded: bool = False):
+class EasySocketClient(object):
+    def __init__(self, host: str, port: int, on_connect: Callable[[EasySocketPeer], None], suffix: bytes = b"\x00\x00\x00\x00\x00", timeout: int = None, threaded: bool = False):
         self.host = host
         self.port = port
         self.on_connect = on_connect
@@ -93,6 +25,6 @@ class EasySockClient(object):
 
         if self.on_connect:
             if self.threaded:
-                threading.Thread(target=self.on_connect, args=[EasySockClientSocket(sock=sock, suffix=self.suffix)], daemon=True).start()
+                threading.Thread(target=self.on_connect, args=[EasySocketPeer(sock=sock, suffix=self.suffix)], daemon=True).start()
             else:
-                self.on_connect(EasySockClientSocket(sock=sock, suffix=self.suffix))
+                self.on_connect(EasySocketPeer(sock=sock, suffix=self.suffix))
